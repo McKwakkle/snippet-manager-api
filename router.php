15b7b9e297179f46d82e1ffe89bbbf5628913fd7
McKwakkle <kellanmcn@gmail.com>
2026-03-18 13:54:30 +0000

    Restored router.php to test out something

--- router.php
+++ router.php
@@ -1,2 +1,10 @@
 <?php
-echo json_encode(['status' => 'router_executing', 'uri' => $_SERVER['REQUEST_URI']]);
+
+$requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$filePath = __DIR__ . '/public' . $requestPath;
+
+if (is_file($filePath)) {
+    return false;
+}
+
+require __DIR__ . '/public/index.php';
